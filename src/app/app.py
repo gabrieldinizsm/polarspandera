2247969778f63a8fd9c040642ed496b3f96e0c17
gabrieldiniz <gabriel.diniz@fengbrasil.com.br>
2024-06-16 18:49:47 -0300

    feat(read_parquet_files): add function to read multiple parquet files

---
+++ src/app/app.py
@@ -0,0 +1,30 @@
+import polars as pl
+import os
+
+
+def read_parquet_files(path: str) -> pl.DataFrame:
+    """
+    Function to read multiple parquet files in a directory.
+
+    It starts listing all parquet files in the directory, after,
+    it tries to read all of them and merging into a single DataFrame, returning it.
+
+    Args:
+        path (str): The directory path to be read.
+
+    Returns:
+        pl.DataFrame: A single DataFrame, consisting in all files merged into one structure.
+    """
+    try:
+        files = [file for file in os.listdir(
+            path) if file.endswith('.parquet')]
+        dfs = [pl.read_parquet(path + file) for file in files]
+        return pl.concat(dfs)
+    except Exception as e:
+        print(f'Error while reading file {e}')
+        raise
+
+
+if __name__ == '__main__':
+    df = read_parquet_files('data/')
+    print(df.head())
